b9d69fff00d438848522142dacd1aac153deed3a
Zelidee <zelideeguemez@gmail.com>
2024-10-14 21:32:12 -0400

    Add project files.

---
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,156 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class MiNavegador
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.webBrowser1 = new System.Windows.Forms.WebBrowser();
+            this.textBoxURL = new System.Windows.Forms.TextBox();
+            this.buttonRefresh = new System.Windows.Forms.Button();
+            this.buttonForward = new System.Windows.Forms.Button();
+            this.buttonBack = new System.Windows.Forms.Button();
+            this.buttonHome = new System.Windows.Forms.Button();
+            this.buttonGo = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // webBrowser1
+            // 
+            this.webBrowser1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.webBrowser1.Location = new System.Drawing.Point(1, 143);
+            this.webBrowser1.MinimumSize = new System.Drawing.Size(20, 20);
+            this.webBrowser1.Name = "webBrowser1";
+            this.webBrowser1.Size = new System.Drawing.Size(1214, 464);
+            this.webBrowser1.TabIndex = 0;
+           /* this.webBrowser1.DocumentCompleted += new System.Windows.Forms.WebBrowserDocumentCompletedEventHandler(this.webBrowser1_DocumentCompleted_1);*/
+            // 
+            // textBoxURL
+            // 
+            this.textBoxURL.Location = new System.Drawing.Point(196, 93);
+            this.textBoxURL.Name = "textBoxURL";
+            this.textBoxURL.Size = new System.Drawing.Size(686, 26);
+            this.textBoxURL.TabIndex = 1;
+            this.textBoxURL.Text = "Escribe la URL aquí";
+            this.textBoxURL.TextChanged += new System.EventHandler(this.textBoxURL_TextChanged);
+            this.textBoxURL.Enter += new System.EventHandler(this.textBoxURL_Enter);
+            this.textBoxURL.Leave += new System.EventHandler(this.textBoxURL_Leave);
+            // 
+            // buttonRefresh
+            // 
+            this.buttonRefresh.Image = global::WindowsFormsApp1.Properties.Resources.w9nmgja9;
+            this.buttonRefresh.Location = new System.Drawing.Point(101, 68);
+            this.buttonRefresh.Name = "buttonRefresh";
+            this.buttonRefresh.Size = new System.Drawing.Size(86, 65);
+            this.buttonRefresh.TabIndex = 6;
+            this.buttonRefresh.UseVisualStyleBackColor = true;
+            this.buttonRefresh.Click += new System.EventHandler(this.buttonRefresh_Click);
+            // 
+            // buttonForward
+            // 
+            this.buttonForward.Image = global::WindowsFormsApp1.Properties.Resources._30right_arrow_icon_outline_style_vector;
+            this.buttonForward.Location = new System.Drawing.Point(1100, 74);
+            this.buttonForward.Name = "buttonForward";
+            this.buttonForward.Size = new System.Drawing.Size(104, 53);
+            this.buttonForward.TabIndex = 5;
+            this.buttonForward.UseVisualStyleBackColor = true;
+            this.buttonForward.Click += new System.EventHandler(this.buttonForward_Click);
+            // 
+            // buttonBack
+            // 
+            this.buttonBack.Image = global::WindowsFormsApp1.Properties.Resources._30LEFT_arrow_icon_outline_style_vector;
+            this.buttonBack.Location = new System.Drawing.Point(996, 74);
+            this.buttonBack.Name = "buttonBack";
+            this.buttonBack.Size = new System.Drawing.Size(104, 53);
+            this.buttonBack.TabIndex = 4;
+            this.buttonBack.UseVisualStyleBackColor = true;
+            this.buttonBack.Click += new System.EventHandler(this.buttonBack_Click);
+            // 
+            // buttonHome
+            // 
+            this.buttonHome.Image = global::WindowsFormsApp1.Properties.Resources.white_home_icon_transparent_png_30;
+            this.buttonHome.Location = new System.Drawing.Point(12, 68);
+            this.buttonHome.Name = "buttonHome";
+            this.buttonHome.Size = new System.Drawing.Size(83, 65);
+            this.buttonHome.TabIndex = 3;
+            this.buttonHome.UseVisualStyleBackColor = true;
+            this.buttonHome.Click += new System.EventHandler(this.buttonHome_Click);
+            // 
+            // buttonGo
+            // 
+            this.buttonGo.Image = global::WindowsFormsApp1.Properties.Resources._30R;
+            this.buttonGo.Location = new System.Drawing.Point(892, 68);
+            this.buttonGo.Name = "buttonGo";
+            this.buttonGo.Size = new System.Drawing.Size(93, 65);
+            this.buttonGo.TabIndex = 2;
+            this.buttonGo.UseVisualStyleBackColor = true;
+            this.buttonGo.Click += new System.EventHandler(this.buttonGo_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(283, 39);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(502, 30);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "Bienvenido/a a tu navegador web personal!";
+            // 
+            // MiNavegador
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1216, 607);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.buttonRefresh);
+            this.Controls.Add(this.buttonForward);
+            this.Controls.Add(this.buttonBack);
+            this.Controls.Add(this.buttonHome);
+            this.Controls.Add(this.buttonGo);
+            this.Controls.Add(this.textBoxURL);
+            this.Controls.Add(this.webBrowser1);
+            this.Name = "MiNavegador";
+            this.Text = "Mi Navegador";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.WebBrowser webBrowser1;
+        private System.Windows.Forms.TextBox textBoxURL;
+        private System.Windows.Forms.Button buttonGo;
+        private System.Windows.Forms.Button buttonHome;
+        private System.Windows.Forms.Button buttonBack;
+        private System.Windows.Forms.Button buttonForward;
+        private System.Windows.Forms.Button buttonRefresh;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Security.Policy;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace WindowsFormsApp1
+{
+    public partial class MiNavegador : Form
+    {
+        public MiNavegador()
+        {
+            InitializeComponent();
+        }
+        private void textBoxURL_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void SetWatermark()
+        {
+            textBoxURL.Text = "Escribe la URL aquí";
+            textBoxURL.ForeColor = System.Drawing.Color.Gray; 
+        }
+
+        private void textBoxURL_Enter(object sender, EventArgs e)
+        {
+            if (textBoxURL.Text == "Escribe la URL aquí")
+            {
+                textBoxURL.Text = ""; 
+                textBoxURL.ForeColor = System.Drawing.Color.Black; 
+            }
+        }
+
+        private void textBoxURL_Leave(object sender, EventArgs e)
+        {
+            if (string.IsNullOrWhiteSpace(textBoxURL.Text))
+            {
+                SetWatermark();
+            }
+        }
+        private void buttonGo_Click(object sender, EventArgs e)
+        {
+            string url = textBoxURL.Text;
+            try
+            {
+                webBrowser1.Navigate(url);
+            }
+            catch (UriFormatException)
+            {
+                MessageBox.Show("La URL ingresada no es válida. Intente nuevamente.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+
+        }
+        private string homeUrl = "https://www.google.com";
+        private void buttonHome_Click(object sender, EventArgs e)
+        {
+            webBrowser1.Navigate(new Uri(homeUrl));
+        }
+
+        private void buttonBack_Click(object sender, EventArgs e)
+        {
+            if (webBrowser1.CanGoBack)
+            {
+                webBrowser1.GoBack();
+            }
+        }
+
+        private void buttonForward_Click(object sender, EventArgs e)
+        {
+            if (webBrowser1.CanGoForward)
+            {
+                webBrowser1.GoForward();
+            }
+        }
+
+        private void buttonRefresh_Click(object sender, EventArgs e)
+        {
+            webBrowser1.Refresh();
+        }
+    }
+}
